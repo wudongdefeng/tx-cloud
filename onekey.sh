a042ef7f2234a5bda9b9dcada8e293c584596ef2
wudongdefeng <32961310+wudongdefeng@users.noreply.github.com>
2022-02-16 14:13:27 +0800

    Create onekey.sh

---
+++ onekey.sh
@@ -0,0 +1,9 @@
+git clone https://github.com:wudongdefeng/tem-update.git ./tem
+cp -r ./config.yml /home/runner/work/tx-cloud/tx-cloud
+cp -r ./configfile/.env /home/runner/work/tx-cloud/tx-cloud
+mv ./serverless.yml ./tem/serverless.yml  
+cp -r ./tem/*.* /home/runner/work/tx-cloud/tx-cloud 
+
+rm -rf /home/runner/work/tx-cloud/tx-cloud/tem        
+npm i
+npm i -g serverless
